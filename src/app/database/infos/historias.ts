272909840d3ed2feeb433791a051dcde4d131793
Renan Ribeiro <73248653+Renan-MF-Ribeiro@users.noreply.github.com>
2021-05-23 23:09:47 -0300

    Update historias.ts

--- src/app/database/infos/historias.ts
+++ src/app/database/infos/historias.ts
@@ -733,6 +733,30 @@ export class Historias{
             <h1 id="avatarkorra#Livro Quatro: Equilíbrio">Livro Quatro: Equilíbrio (平衡)</h1>
             <p>
             Após a insurreição do Lótus Vermelho, Korra partiu para a Tribo da Água do Sul para se recuperar e o Time Avatar seguiu em frente com suas vidas. Três anos depois, em 174 AG, a ex-capitã da guarda de Zaofu, Kuvira, estava viajando pelo Reino da Terra com Bolin, Varrick e seu noivo, Baatar Jr., em uma missão para reunir a nação depois que ela caiu em desordem após a morte da rainha Hou-Ting. Quando ela deveria abdicar do controle para o recém-coroado Rei da Terra Wu, ela denunciou sua autoridade e se autoproclamou governante do Império da Terra. Enquanto isso, Korra conseguiu se curar fisicamente, embora ela ainda esteja enfrentando um bloqueio emocional causado por traços residuais de mercúrio. Com os problemas chegando ao Reino da Terra, a Equipe Avatar se dividiu e o paradeiro do Avatar é desconhecido, a busca pelo equilíbrio começa novamente.</p>
+
+            <h1 id="avatarkorra#Personagens">Personagens</h1>
+           <h2>Personagens principais</h2>
+           
+        <li><b>Korra (Janet Varney):</b> É o Avatar que sucede imediatamente a Aang e o principal protagonista da série. Ela é uma dobradora talentosa, ávida por aprender. Impulsiva e rebelde, Korra saboreia seu papel como "O Escolhido". Ela também tem um cachorro urso polar chamado Naga como seu companheiro animal.</li>
+<li>
+        <b>Mako (David Faustino):</b> É um dobrador de fogo que cresceu nas ruas de Cidade República com seu irmão mais novo, Bolin, que o apresentou a Korra. Alto, magro e bonito, ele era um participante habilidoso em pro-dobra e ex-capitão dos Furões de Fogo, antes de se tornar um policial.</li>
+<li>
+        <b>Bolin (P. J. Byrne):</b> É um dobrador de terra que, junto com seu irmão Mako, se torna amigo de Korra. Ele compartilha um vínculo profundo com seu irmão e mantém um furão de fogo chamado Pabu. Bolin está entusiasmado e otimista e, como um pró-dobrador, gosta de toda a atenção que recebe de suas fãs. Seu jeito carismático lhe rendeu um trabalho como ator.</li>
+<li>
+        <b>Asami Sato (Seychelle Gabriel):</b> É filha de Hiroshi Sato. Asami viveu uma vida de luxo, mas apesar de suas roupas extravagantes e maneiras educadas, ela tem um lado difícil. Ela é uma motorista experiente e teve um extenso treinamento em autodefesa. Asami é uma "grande fã" de pro-dobra, e depois da prisão de seu pai, ela assume a empresa dele.</li>
+<li>
+        <b>Tenzin (J. K. Simmons):</b> É o filho mais novo de Aang e Katara e um mestre dobra de ar. Um homem calmo e sério, ele carrega um grande fardo para proteger não apenas Korra, mas também a Cidade República. Tenzin mora em Ilha do templo do Ar com sua esposa, Pema, e seus quatro filhos, Jinora, Ikki, Meelo e Rohan.</li>
+<li>
+        <b>Lin Beifong (Mindy Sterling):</b> Lidera a Força Policial Metalbending da Cidade da República e é filha de Toph Beifong. Ela é dura e intransigente, acreditando que a única maneira de manter a paz e a estabilidade na cidade é através do uso do poderio militar.</li>
+
+        <h2>Personagens do Livro Um</h2>
+        <li>
+        <b>Amon (Steve Blum):</b> É o principal antagonista do primeiro livro da série. Ele é o líder carismático e misterioso dos Igualitario, cujo objetivo final é criar igualdade por meio de uma revolução anti-dobra. Ele usa uma máscara para esconder o rosto. Um lutador ágil e furtivo, Amon não tem medo de enfrentar seus oponentes, independente de suas habilidades de dobra, e também representa um grande perigo para os dobradores, sendo capaz de tirar sua dobra.</li>
+        <li>
+        <b>O Tenente (Lance Henriksen):</b> É o segundo em comando de Amon. Ele é um lutador intenso e disciplinado que aumenta suas habilidades incríveis com dois bastões de kali eletrificados.</li>
+        <li>
+        <b>Hiroshi Sato (Daniel Dae Kim):</b> É um empresário com grande personalidade. A linhagem de sua família remonta aos primeiros colonos da Nação do Fogo. Hiroshi é um rico industrial, produzindo Satomobiles em suas fábricas da Future Industries. Ele mais tarde é revelado estar trabalhando com os Equalists, fabricando a tecnologia que eles usam para lutar, como vingança contra todos os dobradores pelo assassinato de sua esposa, Yasuko.
+Tarrlok (Dee Bradley Baker) é o antagonista secundário do primeiro livro da série. Ele é o representante da Tribo da Água do Norte no Conselho da República Unida e também atua como seu presidente. Tarrlok busca lutar contra a Revolução Anti-Dobra com violência ao invés de negociação pacífica e está disposto a ir ao extremo para vencer o conflito contra os igualistas, mesmo que isso signifique atiçar as chamas da agressão e arriscar a vida de pessoas inocentes.
             
             `
 
